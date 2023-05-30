92c11aa664c5479a131c9d5d0c5acc3285a2f519
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-05-29 18:45:19 -0600

    Actuali

--- Introduccion/operadores.js
+++ Introduccion/operadores.js
@@ -12,3 +12,32 @@
 
 const resultado = 10 + 10;
 
+/* Operadores de asignación
++= Suma un numero al valor de una varibale
+-= Resta un numero al valor de una variable
+*+ Multiplica un numero al valor de una variable
+/= Resta un numero al valor de una variable
+%= Obtiene el sobrante de una division y lo asigna  a la variable
+*/
+
+let numero = 10;
+numero += 4;
+
+console.log(numero)
+
+/*Operadores de comparacion
+Nos permiten comparacion
+== Igual al valor
+=== Igual al valor y tipo de valor
+!=
+!== Diferente en valor y diferente en tipo de dato
+> Mayor que
+< Menor que
+>= Mayor o igual que 
+<= Menor o igual que
+? Operador ternario (Es una especie de condicional pero en una sola linea de codigo*/
+
+// Ejemplo de como usar operador ternario
+const ternario = 7 > 10 ? "El primer numero es mayor" : "El segundo numero es mayor"
+
+console.log(ternario)
