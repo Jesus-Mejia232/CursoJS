48b38daf3a357a3183a195034a67eac792c55644
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-05-29 21:10:20 -0600

    Actuali

---
+++ Introduccion/templatesStrings.js
@@ -0,0 +1,10 @@
+const nombre =  "Jesus Mejia";
+const edad = 17;
+const pais = "Honduras";
+
+// Forma no optima
+console.log("La persona es: " + nombre + " tiene " + edad + " y es de " + pais)
+
+
+// Forma no optima
+console.log(`La persona es ${nombre} tiene ${edad} y es de ${pais}`)
