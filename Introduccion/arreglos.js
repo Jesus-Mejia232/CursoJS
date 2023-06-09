cdb1b8f194a001f0e58693d7c7f48244101b2a3d
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-06-08 18:46:13 -0600

    Actuali

--- Introduccion/arreglos.js
+++ Introduccion/arreglos.js
@@ -14,3 +14,5 @@ console.log("El arreglo tiene:" + colores.length + " Colores");
 colores.push("Azul"); // Push siver para agregar un elemento de ultimo al arreglo, sin necesidad de poner el indice en el que va
 
 console.log(colores)
+
+// Crear 
--- Introduccion/scopeLocal.js
+++
@@ -1,5 +0,0 @@
-/* Scope local o Variable Locales:
-
--Son variables declaradas dentro de una funcion.
--Solo podemos acceder a ellas desde dentro de la funcion.
-*/
