22e270bfdf0f8166dae943fe886224cff88bd005
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-07-05 09:38:33 -0600

    Actuali

--- Introduccion/avanzando/metodosEstaticos.js
+++ Introduccion/avanzando/metodosEstaticos.js
@@ -1,14 +1,14 @@
 class User {
-    constructor(nombre, correo){
+    constructor(nombre,correo){
         this.nombre = nombre;
         this.correo = correo;
-    };
-
-    // Con la palabra clave "static" estamos haciendo posible acceder al metodo o propiedad sin tener que definir una nueva variable
-    static borrar(id_user){
-        console.log(`El usuario con el id "${id_user}" ha sido eliminado de la base de datos`);
-    };
-};
+    }
 
 
-User.borrar(1);
+    borrarUser(id_user){
+        console.log(`El usuario con el id: ${id_user} ha sido eliminado`);
+    }
+}
+
+const variable = new User("jesus","correo@correo.com");
+variable.borrarUser(1);
